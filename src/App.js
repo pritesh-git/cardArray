2c59e31fcd1718bac8959509c1a94d2ed041c571
preet-esh <pritesh.bhati59@gmail.com>
2020-06-20 13:42:36 +0530

    design upgrade

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import fillVal from './fillVal.json'
+import 'bootstrap/dist/css/bootstrap.css';
+import { IoIosThumbsUp,IoIosText,IoIosSend } from "react-icons/io";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+<div class="row row-cols-1 row-cols-md-4 m-3" style={{border:"1px red dotted"}}>
+    {
+      fillVal.fills.map((val) => (
+      <div class="card mb-3 m-4 p-2">
+      <img class="card-img-top h-50" src={val.Num} alt="Click to load" />
+      <div class="card-body">
+
+        <h3 class="card-title">{val.Name}</h3>
+        <p class="card-text"><span class="badge badge-info">{val.Post}</span></p>
+        <p class="d-flex justify-content-between">
+          <button class="btn btn-link"><IoIosThumbsUp/> {val.Like} Likes</button>
+          <button class="btn btn-link"><IoIosText/> {val.Comment} Comment</button>
+          <button class="btn btn-link"><IoIosSend/> {val.Share} Share</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
+      ))
+    }
+
+  </div>  
   );
 }
 
